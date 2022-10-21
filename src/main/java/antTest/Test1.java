ec57a01269223323765c14669e60ef5b0144e66a
KTE-061 <KTE-061@DESKTOP-K1I5B3L>
2022-10-21 12:25:11 +0900

    exam1017 커밋

--- src/main/java/antTest/Test1.java
+++ src/main/java/antTest/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 
 
-		System.out.println("이 프로그램은 Test1입니다");
+		System.out.println("이 프로그램은 Test1입니다"); 
 
 	}
 
--- src/main/java/antTest/Test2.java
+++ src/main/java/antTest/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 
 		
-		System.out.println("이 프로그램은 Test2입니다");
+		System.out.println("이 프로그램은 Test2입니다"); 
 
 	}
 
