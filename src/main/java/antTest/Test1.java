db72571bef094633ad7a7777d8814d32f60a0685
KTE-061 <KTE-061@DESKTOP-K1I5B3L>
2022-10-21 12:20:56 +0900

    exam1017 커밋

---
+++ src/main/java/antTest/Test1.java
@@ -0,0 +1,12 @@
+package antTest;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("이 프로그램은 Test1입니다");
+
+	}
+
+}
---
+++ src/main/java/antTest/Test2.java
@@ -0,0 +1,12 @@
+package antTest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("이 프로그램은 Test2입니다");
+
+	}
+
+}
